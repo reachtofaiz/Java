cec521beaf6a578113f158192a07d206330a633f
reachtofaiz246@gmail.com <reachtofaiz246@gmail.com>
2024-08-28 16:14:26 +0530

    Adding condition statements of java

---
+++ Condition.java
@@ -0,0 +1,18 @@
+public class Condition{
+
+    public static void main(String[] args) {
+        int x = 16;
+        if (x<=12) {
+        System.out.println("Good Morning");
+        }
+        else if (x<=15) {
+        System.out.println("Good afternoon");
+        }
+        else if (x<=19) {
+        System.out.println("Good Evening");
+        }
+        else if (x<=24){
+            System.out.println("Good night ");
+        }
+    }
+}
