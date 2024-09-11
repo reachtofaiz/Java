e21489b0cc07fe1060c06bd6f7ba1615761da0df
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2024-09-11 13:36:48 +0000

    Adding some pattern question in pattern folder..

---
+++ Pattern/Half_Triangle.java
@@ -0,0 +1,11 @@
+public class Half_Triangle{
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
