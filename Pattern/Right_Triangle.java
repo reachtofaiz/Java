1d0c2dac5fbc4dfdb96fcf1710661d39a2faeee1
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2024-09-13 14:41:53 +0000

    Adding right triangle pattern question in pattern folder..

---
+++ Pattern/Right_Triangle.java
@@ -0,0 +1,13 @@
+public class Right_Triangle{
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+
+
+        }
+    }
+}
