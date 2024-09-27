a2063d50e8d468b5f1feff65c1afb6ae9d2cb239
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2024-09-27 18:02:33 +0000

    adding Largest element method 1 in array folder

---
+++ Array/Largest_element.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Largest_element{
+    public static void main(String[] args) {
+
+        int arr[] = {5,4,8,1,3};
+         
+        // int arr[] = {9,5,1,3,4};
+    }
+
+    static int Largest_element(arr[]){
+
+    }
+}
