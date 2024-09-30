14d465ac1a733d952c94333c9011fd85000b83b0
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2024-09-30 15:41:20 +0000

    Adding recursive approach in largest element in array folder.

--- Array/Largest_element.java
+++ Array/Largest_element.java
@@ -1,14 +1,40 @@
+// Method number 1
+/*
 import java.util.Arrays;
+
+public class Largest_element {
+
+    public static void main(String[] args) {
+
+        // int[] arr = {5, 4, 8, 1, 3};
+        // int[] arr = {63,2,5,8,55};
+        int[] arr = { 5, -2, 23, 7, 87, -42};
+
+        Arrays.sort(arr);
+        System.out.print(arr[arr.length - 1] );
+        System.out.println("");
+    }
+}
+*/
+
+// method 2. This is approach is called recursive approach
 
 public class Largest_element{
     public static void main(String[] args) {
+        // int[] arr = {8, 2, 6, 10, 7, 9};
+        // int[] arr = {63,2,5,8,55};
+        int[] arr = { 5, -2, 23, 7, 87, -42};
+    
+        int max;
+        
+        max = arr[0];
+        // System.out.println(max);
 
-        int arr[] = {5,4,8,1,3};
-         
-        // int arr[] = {9,5,1,3,4};
-    }
-
-    static int Largest_element(arr[]){
-
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+            System.out.println("The largest element is " + max);
     }
 }
