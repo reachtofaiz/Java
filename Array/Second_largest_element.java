0c64f8d3553e8d36ca7c76bbc1a9ca59c1599325
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2024-10-01 14:23:18 +0000

    Adding method 1 to find second largest element.

---
+++ Array/Second_largest_element.java
@@ -0,0 +1,25 @@
+// Method 1  simple solution without use of any mind
+
+/*
+import java.util.Arrays;
+
+public class Second_largest_element{
+    public static void main(String[] args) {
+        int[] arr = {9,2,3,10,45};
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("");
+
+        System.out.println("The second largest element is " + arr[arr.length - 2]);
+    }
+}   
+
+*/
+
+
+
